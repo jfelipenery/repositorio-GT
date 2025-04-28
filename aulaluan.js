79c85c702341feaac05564e13cdd80238ffe3ea0
unknown <joaopenha0520@digitalcollege.internal>
2025-04-28 16:43:55 -0300

    atualização

---
+++ aulaluan.js
@@ -0,0 +1,65 @@
+// let nome = "joão";
+// let idade = 33;
+// let nacionalidade = "Brasileiro";
+
+// console.log("Eu me chamo "+ nome +" tenho " + idade + " anos " + "e sou " + nacionalidade);
+
+// let eAluno = false;
+// let curso = "fullstack Geração Tech"
+
+// console.log(" ");
+
+// if (eAluno == true ){
+//     console.log("olá me chamo "+ nome + " e sou Aluno do curso " + curso);
+// }
+
+// else  {
+//     console.log("olá me chamo "+ nome + " e sou Professor do curso " + curso);
+// }
+
+// console.log(frutas[0])
+// console.log(frutas.length);
+// console,log (frutas[frutas.length - 1]);
+
+// let frutas = ["laranja", "maça", "uva"]
+// for (let i = 0; i <frutas.length; i++){
+//     console.log(frutas[i]);
+// }
+
+// function saudacao (){
+//     console.log("Olá Mundo!");
+// }
+
+// saudacao();
+
+// function soma(numero1, numero2) {
+//     console.log(numero1+numero2);
+
+// }
+
+// soma(4,5)
+
+// function soma(numero1, numero2) {
+//     console.log(numero1);
+//     console.log(numero2);
+//     let soma = numero1 + numero2;
+
+//     console.log("Resultado:" + soma);
+
+// }
+
+// soma(4,5) 
+
+// function saudacao(nome){
+//     console.log(" olá " + nome);
+// }
+
+// saudacao("joaquim")
+
+
+
+function saudacao(nome, idade, nacionalidade) {
+    console.log("olá " + nome + idade + nacionalidade);
+
+}
+saudacao("joao ", "31 anos" ," brasileiro")
---
+++ aulamisael.js
@@ -0,0 +1,104 @@
+// let carro = {modelo: "civic", ano: 2024, marca: "honda" }
+
+// console.log(carro);
+
+
+// let lula = {ladrão: "lavagem de", prisão: "condenado", inocentado: "crimes "}
+
+// console.log (lula)
+
+// let carros = [
+// {modelo: "civic", ano: 2024, marca: "honda"},
+// {modelo: "siena", ano: 2010, marca: "fiat"},
+// {modelo: "marea", ano: 2024, marca: "fiat"}
+
+// ]
+
+// console.log(carros[1]);
+
+// let filmes = [
+//     {titulo: "titanic", genero: "drama", ano: 1900},
+//     {titulo: "jason", genero: "terror", ano: 1989},
+//     {titulo: "os ursinhos carinhosos", genero: "fantasia", ano: 1994}
+    
+//     ]
+    
+//     console.log(filmes[0].titulo);
+
+// function criarPessoa(nome, idade, genero) {
+//     return {
+//         nome: nome,
+//         idade: idade,
+//         genero: genero
+//     }
+      
+// }
+// let pessoa1 = criarPessoa ("lanlton" , 24, "não binario")
+
+// console.log(pessoa1.genero);
+
+// function nomeFilme(filme, genero, ano) {
+//     return {
+//         filme: filme,
+//         genero: genero,
+//         ano: ano
+//     };
+      
+// }
+// let filme = nomeFilme ("titanic" , "drama", 1990)
+
+// console.log(filme.genero);
+
+// alert("welcome")
+
+// prompt("Coloca teu nome aqui!")
+
+// confirm("Deseja sair? clique no OK!")
+
+// let nome = prompt("qual o seu nome?")
+// alert("Bem vindo: " + nome);
+
+
+// let nome = prompt("qual o seu nome?");
+// let confirma = confirm("seu nome é: " + nome + "?");
+// if(confirma){
+//     alert("Nome confirmado com sucesso")
+// }
+// else{"nome não confirmado"};
+
+
+
+    // let nome = prompt("qual o seu nome?");
+    // alert("nome confirmado!")
+    
+    // let idade = prompt("qual sua idade?")
+    // alert("idade confirmada!");
+    
+    // let filme = prompt("qual o seu filme favorito?")
+    // alert("filme confirmado!")
+
+    // alert("seu nome é: " + nome + " sua idade é: " + idade + " seu filme é: "+ filme)
+
+    
+        let nome = prompt("insira o seu nome");
+        let idade = prompt("insira sua idade");
+        let filmeFavorito = prompt("Digite seu filme");
+        
+
+
+    let usuario = { 
+        nome: nome,
+        idade: idade,
+        filmeFavorito: filmeFavorito
+    }
+        alert(
+            "seu nome é? " + nome +
+            "sua idade é? " + idade +
+            "seu filme favorito é? " + filmeFavorito,
+            
+        )
+
+        
+
+
+
